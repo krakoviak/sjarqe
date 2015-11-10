e0433c1a837469ba56711388ad5a22e6631c9a6f
Khaidukov Kirill <kirill.khaidukov@lamoda.ru>
2015-11-10 17:13:42 +0300

    reorganized a bit

--- lib/sjarqe.rb
+++ lib/sjarqe.rb
@@ -1,5 +1,11 @@
-VOWEL = {:nil => 5, :a => 6, :ya => 3, :e => 5, :ye => 2, :u => 6, :i => 3, :o => 7, :yo => 2, :aa => 0, :ee => 0, :uu => 0, :ii => 0, :oo => 0, :ai => 3, :ei => 3, :yei => 1, :oi => 2, :ou => 2, :au => 2}
-CONSONANT = {:nil => 5, :y => 4, :q => 4, :kh => 4, :w => 3, :r => 7, :R => 5, :t => 7, :p => 6, :s => 8, :d => 8, :f => 3, :g => 4, :x => 4, :k => 6, :l => 6, :z => 7, :v => 6, :b => 3, :n => 5, :m => 5, :sj => 4, :dj => 4, :ch => 4, :th => 6, :dh => 6, :j => 2, :ts => 5, :dz => 5}
+NIL_VOWEL     = {:nil => 5}
+SHORT_VOWELS  = {:a => 6, :ya => 3, :e => 5, :ye => 2, :u => 6, :i => 3, :o => 7, :yo => 2}
+LONG_VOWELS   = {:aa => 0, :ee => 0, :uu => 0, :ii => 0, :oo => 0}
+DIPHTONGS     = {:ai => 3, :ei => 3, :yei => 1, :oi => 2, :ou => 2, :au => 2}
+
+VOWELS = NIL_VOWEL.merge(SHORT_VOWELS).merge(LONG_VOWELS).merge(DIPHTONGS)
+
+CONSONANTS = {:nil => 9, :y => 4, :q => 4, :kh => 4, :w => 3, :r => 7, :R => 5, :t => 7, :p => 6, :s => 8, :d => 8, :f => 3, :g => 4, :x => 4, :k => 6, :l => 6, :z => 7, :v => 6, :b => 3, :n => 5, :m => 5, :sj => 4, :dj => 4, :ch => 4, :th => 6, :dh => 6, :j => 2, :ts => 5, :dz => 5}
 
 #TEXT = {:verse => %w(a4), :structure => [:verse]}
 #TEXT = {:verse => %w(a4 a4 a4 a4), :structure => [:verse, :line, :verse]}
@@ -15,10 +21,10 @@ CLOSED_SYLLABLE_FREQUENCY = 5
 LONGER_WORDS_FREQUENCY = 5
 
 def generate structure
-  assimilations = load 'assimilation_rules'
-  palatalizations = load 'palatalizations'
-  palatalizers = load 'palatalizers'
-  cons_rhyme = load 'consonant_rhyme'
+  assimilations = load('assimilation_rules')
+  palatalizations = load('palatalizations')
+  palatalizers = load('palatalizers')
+  cons_rhyme = load('consonant_rhyme')
 
   structure[:structure].each do |text_block|
     if text_block == :line
@@ -28,8 +34,8 @@ def generate structure
       rhymes = {}
       structure[text_block].each do |line_type|
         rhyme_key = line_type.scan(/[a-z]/).join.to_sym
-        rhymes[rhyme_key] ||= { :c2 => get(CONSONANT), :v2 => get(VOWEL), :c1 => get(CONSONANT), :v1 => get(VOWEL), :c0 => rand(2) > 0 ? get(CONSONANT) : nil }
-        #puts rhymes[rhyme_key]
+        rhymes[rhyme_key] ||= { :c2 => get_consonant, :v2 => get_vowel, :c1 => get_consonant, :v1 => get_vowel, :c0 => rand(2) > 0 ? get_consonant : nil }
+        # puts rhymes[rhyme_key]
 
         vowel_count = line_type.scan(/\d+/).join.to_i
         rhyme = rhymes[rhyme_key]
@@ -48,12 +54,12 @@ def generate structure
         while i < vowel_count
 
           if (word_start && cons_sequence == 0) || (cons_sequence < MAX_CONSONANT_SEQUENCE && !word_start)
-            consonant = (vowel_count - i) < 3 ? cons_rhyme[rhyme["c#{(vowel_count - i)}".to_sym].to_s].split.sample : get(CONSONANT)
+            consonant = (vowel_count - i) < 3 ? cons_rhyme[rhyme["c#{(vowel_count - i)}".to_sym].to_s].split.sample : get_consonant
             #puts "before: #{consonant}"
             line << consonant.to_s
           end
 
-          vowel = vowel_count - i <= 2 ? rhyme["v#{vowel_count - i}".to_sym] : get(VOWEL)
+          vowel = vowel_count - i <= 2 ? rhyme["v#{vowel_count - i}".to_sym] : get_vowel
 
           i += 1
           vowels_in_last_word += 1
@@ -81,7 +87,7 @@ def generate structure
                   elsif vowel_count - i == 0
                     ''
                   else
-                    get(CONSONANT)
+                    get_consonant
                   end
             line << add.to_s
             cons_sequence += 1
@@ -125,12 +131,20 @@ def generate structure
   end
 end
 
-def get letters
+def get(letters)
   random_letter = letters.keys.sample until random_letter && letters[random_letter] * 10.0 > rand(100) && random_letter != :nil
   random_letter
 end
 
-def load file
+def get_vowel
+  get(VOWELS)
+end
+
+def get_consonant
+  get(CONSONANTS)
+end
+
+def load(file)
   hash = {}
   IO.foreach("./#{file}") do |line|
     if line.strip[0] != '#' && line.include?('=>')
@@ -141,4 +155,4 @@ def load file
   hash
 end
 
-generate TEXT
+generate(TEXT)
